ef45a846bb3c24e4f4c5ea13722570108f786514
Michael Campbell <michael.campbell14@gmail.com>
2020-10-13 12:16:05 -0400

    lesson 5, routing and navigation in gatsby

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function SliceMastersPage() {
   return (
     <div>
+      <Nav />
       <p> Hey! I'm the EEEEE slice masters page</p>
     </div>
   );
