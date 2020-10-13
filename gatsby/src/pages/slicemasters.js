c3ac8b8166d557d00972a00f11649d4798164ee0
Michael Campbell <michael.campbell14@gmail.com>
2020-10-13 12:39:30 -0400

    lesson 6, creating layouts in gatsby

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Nav from '../components/Nav';
 
 export default function SliceMastersPage() {
   return (
-    <div>
-      <Nav />
+    <>
       <p> Hey! I'm the EEEEE slice masters page</p>
-    </div>
+    </>
   );
 }
