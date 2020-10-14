651a3b7d910f3137337b136354fbb432141db47e
Michael Campbell <michael.campbell14@gmail.com>
2020-10-14 12:35:16 -0400

    lesson 10, styling our Layout

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -19,6 +19,7 @@ const NavStyles = styled.nav`
     grid-template-columns: 1fr 1fr auto 1fr 1fr;
     grid-gap: 2rem;
     align-items: center;
+    margin-top: -6rem;
   }
 
   li {
@@ -44,6 +45,9 @@ const NavStyles = styled.nav`
     &:hover {
       color: var(--red);
     }
+    // &[aria-current='page'] {
+    //   color: var(--red);
+    // }
   }
 `;
 
