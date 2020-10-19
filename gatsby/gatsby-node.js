56334a4064cccae5a947cf1c10926bb9e91b94dd
Michael Campbell <michael.campbell14@gmail.com>
2020-10-19 13:25:37 -0400

    lesson 23, dynamically creating pages with gatsby-node

---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,39 @@
+import path from 'path';
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+  // 1. get a template for this page
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+  // 2. query all pizzas
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  console.log(data);
+  // 3. loop over each pizza and create a page for that pizza
+  data.pizzas.nodes.forEach((pizza) => {
+    actions.createPage({
+      // What is the URL for this new page??
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+      context: {
+        slug: pizza.slug.current,
+      },
+    });
+  });
+}
+
+export async function createPages(params) {
+  // create pages dynamically
+  // 1. pizzas
+  await turnPizzasIntoPages(params);
+  // 2. toppings
+  // 3. slicemasters
+}
