3435cb3d11dd15547894ae313851dc9c56361be5
Michael Campbell <michael.campbell14@gmail.com>
2020-10-19 14:29:38 -0400

    lesson 25, dynamically creating toppings pages

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,9 +1,10 @@
-import path from 'path';
+import path, { resolve } from 'path';
+import fetch from 'isomorphic-fetch';
 
 async function turnPizzasIntoPages({ graphql, actions }) {
-  // 1. get a template for this page
+  // 1. Get a template for this page
   const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
-  // 2. query all pizzas
+  // 2. Query all pizzas
   const { data } = await graphql(`
     query {
       pizzas: allSanityPizza {
@@ -16,8 +17,7 @@ async function turnPizzasIntoPages({ graphql, actions }) {
       }
     }
   `);
-  console.log(data);
-  // 3. loop over each pizza and create a page for that pizza
+  // 3. Loop over each pizza and create a page for that pizza
   data.pizzas.nodes.forEach((pizza) => {
     actions.createPage({
       // What is the URL for this new page??
@@ -30,10 +30,43 @@ async function turnPizzasIntoPages({ graphql, actions }) {
   });
 }
 
+async function turnToppingsIntoPages({ graphql, actions }) {
+  // 1. Get the template
+  const toppingTemplate = path.resolve('./src/pages/pizzas.js');
+  // 2. query all the toppings
+  const { data } = await graphql(`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+        }
+      }
+    }
+  `);
+  // 3. createPage for that topping
+  data.toppings.nodes.forEach((topping) => {
+    actions.createPage({
+      path: `topping/${topping.name}`,
+      component: toppingTemplate,
+      context: {
+        topping: topping.name,
+        // TODO Regex for Topping
+        toppingRegex: `/${topping.name}/i`,
+      },
+    });
+  });
+  // 4. Pass topping data to pizza.js
+}
+
 export async function createPages(params) {
-  // create pages dynamically
-  // 1. pizzas
-  await turnPizzasIntoPages(params);
-  // 2. toppings
-  // 3. slicemasters
+  // Create pages dynamically
+  // Wait for all promises to be resolved before finishing this function
+  await Promise.all([
+    turnPizzasIntoPages(params),
+    turnToppingsIntoPages(params),
+  ]);
+  // 1. Pizzas
+  // 2. Toppings
+  // 3. Slicemasters
 }
--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -19,7 +19,7 @@ const ToppingStyles = styled.div`
       background: white;
       padding: 2px 5px;
     }
-    .active {
+    &[aria-current='page'] {
       background: var(--yellow);
     }
   }
@@ -55,7 +55,7 @@ function countPizzasInToppings(pizzas) {
   return sortedToppings;
 }
 
-export default function ToppingsFilter() {
+export default function ToppingsFilter({ activeTopping }) {
   // get a list of all the toppings
   // get a list of all the pizzas with their toppings
   const { toppings, pizzas } = useStaticQuery(graphql`
@@ -84,8 +84,16 @@ export default function ToppingsFilter() {
   // link it up...
   return (
     <ToppingStyles>
+      <Link to="/pizzas">
+        <span className="name">All</span>
+        <span className="count">{pizzas.nodes.length}</span>
+      </Link>
       {toppingsWithCounts.map((topping) => (
-        <Link key={topping.id} to={`/topping/${topping.name}`}>
+        <Link
+          className={topping.name === activeTopping ? 'active' : ''}
+          key={topping.id}
+          to={`/topping/${topping.name}`}
+        >
           <span className="name">{topping.name}</span>
           <span className="count">{topping.count}</span>
         </Link>
