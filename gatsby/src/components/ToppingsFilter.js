493619268ef9ef9b57108222f48294561670f0f4
Michael Campbell <michael.campbell14@gmail.com>
2020-10-22 14:46:17 -0400

    lesson 52, making the website responsive

--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -9,12 +9,14 @@ const ToppingStyles = styled.div`
   margin-bottom: 4rem;
   a {
     display: grid;
-    padding: 5px;
     grid-template-columns: auto 1fr;
-    grid-gap: 5px;
-    background: var(--grey);
+    grid-gap: 0 1rem;
     align-items: center;
+    padding: 5px;
+    background: var(--grey);
     border-radius: 2px;
+    text-decoration: none;
+    font-size: clamp(1.5rem, 1.4vw, 2.5rem);
     .count {
       background: white;
       padding: 2px 5px;
--- gatsby/src/styles/OrderStyles.js
+++ gatsby/src/styles/OrderStyles.js
@@ -19,12 +19,12 @@ const OrderStyles = styled.form`
   .maplesyrup {
     display: none;
   }
-  /*@media (max-width: 900px) {
+  @media (max-width: 900px) {
     fieldset.menu,
     fieldset.order {
       grid-column: span 2;
     }
-  } */
+  }
 `;
 
 export default OrderStyles;
